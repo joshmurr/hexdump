34fa85caada714cc1e1b8f6206a349bd61e794d8
Josh Murr <joshuadavidmurr@gmail.com>
2019-09-23 19:35:14 +0100

    Offset working

--- test2.c
+++ test2.c
@@ -3,8 +3,6 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-    int i, c, offset;
-    
     if(argc != 2){
         fprintf(stderr, "Usage view_bytes FILE.ext\n");
         return 1;
@@ -22,30 +20,21 @@ int main(int argc, char *argv[]){
     fseek(inputFile, 0L, SEEK_END);
     int size = ftell(inputFile);
     fseek(inputFile, 0L, SEEK_SET);
-    
-    char line[18];
 
-    i = offset = 0;
+    int lineLen = 17;
+    char line[lineLen];
+    int i, c, offset = 0;
 
     c = getc(inputFile);
     while (c != EOF) {
-
-        //Print initial offset
-        // printf("%08x  ", i);
+        i = 0;
         line[i]  = (char)offset;
 
-        for (i=1; i<18; ++i){
-           //End of line
-            if (i == 17) {
-                // printf("%c", c);
-                //Print offset as start of new line
-                line[i] = '\n';
-            } else {
-                line[i] = c;
-                c = getc(inputFile);
-                if(c == EOF){
-                    line[i] = ' ';
-                }
+        for (i=1; i<lineLen; ++i){
+            line[i] = c;
+            c = getc(inputFile);
+            if(c == EOF){
+                line[i] = ' ';
             }
             offset++;
         } 
@@ -53,36 +42,18 @@ int main(int argc, char *argv[]){
         do {
             printf("%02x ", line[j]);
             j++;
-        } while(j<18);
+        } while(j<lineLen);
         printf("%s", "  |");
         int k=0;
         do {
-            printf("%c", line[k]);
+            if(line[k] == '\n') printf("%c", ' ');
+            else printf("%c", line[k]);
             k++;
-        } while(k<18); 
+        } while(k<lineLen); 
+        printf("%s", "|\n");
 
     };
-
-
     fclose(inputFile);
 
     return 0;
 }
-
-
-/*
-*            //Print char in hex
-*            printf("%02x", c);
-*            //End of line
-*            if (i % 16 == 15) {
-*                printf("%c", c);
-*                //Print offset as start of new line
-*                printf("\n%08x  ", i+1);
-*            } else if (i % 8 == 7) {
-*                //Print double space after 8 bytes
-*                printf("%*c", 2, ' ');
-*            } else {
-*                //Space between each byte
-*                putchar(' ');
-*            }
-*/
